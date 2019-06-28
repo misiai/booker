0dd4baffe33f89776694bfa09ed8acae6e65cac8
夏宽亮 <991418182@qq.com>
2019-06-28 22:21:54 +0800

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/28 22:08
+# @Author  : 无道
+# @Email   : admin@misiai.com
+# @File    : main.py
+# @Software: PyCharm
+# @description: 
+import requests
+
+
+def process():
+    print("this is book cather!")
+
+
+def main():
+    process()
+
+
+if __name__ == '__main__':
+    main()
